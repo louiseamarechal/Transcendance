dbcfd5bde10a7658df624d494f87ad6a79dcb418
louiseameliemarechal <louiseamelie2@gmail.com>
2023-05-23 17:26:59 +0200

    byebye Vite

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+return (
+  <div className='h-screen homepage flex flex-col justify-center items-center gap-y-20'>
+    <div>
+      <h1>Welcome to our Pong Game : <br/> Transcendance</h1>
+    </div>
+    <div className='signing-option flex flex-col justify-center content-center gap-y-10'>
+        <button>Sign in</button>
+        <button>Sign in with 42</button>
+    </div>
+  </div>
+)
+}
+
+export default App
