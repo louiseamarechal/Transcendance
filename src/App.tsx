36edba28e9225df492049c08e353bbb528a17e16
louiseameliemarechal <louiseamelie2@gmail.com>
2023-05-23 18:12:17 +0200

    added animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
+import Reveal from './components/reveal.tsx'
 import './App.css'
 
 function App() {
 
 return (
-  <div className='h-screen homepage flex flex-col justify-center items-center gap-y-20'>
-    <div>
-      <h1>Transcendance</h1>
+  <Reveal>
+    <div className='h-screen homepage flex flex-col justify-center items-center gap-y-20'>
+      <div>
+        <h1>Transcendance</h1>
+      </div>
+      <div className='signing-option flex flex-col justify-center content-center gap-y-10'>
+          <button>Sign in</button>
+          <button>Sign in with 42</button>
+      </div>
     </div>
-    <div className='signing-option flex flex-col justify-center content-center gap-y-10'>
-        <button>Sign in</button>
-        <button>Sign in with 42</button>
-    </div>
-  </div>
+  </Reveal>
 )
 }
 
