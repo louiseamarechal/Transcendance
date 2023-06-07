4935a665c647ec5be3b60bff60f78e8fa336d69b
JBouyer <jbouyer@student.42.fr>
2023-06-07 17:56:43 +0200

    net a aligner

--- front/src/pages/Game.tsx
+++ front/src/pages/Game.tsx
@@ -1,14 +1,25 @@
 // import React from 'react'
 import NavBar from '../components/NavBar.tsx';
+import '../style/components/net.css'
+import '../style/components/buttons.css'
+import '../style/pages/Game.css'
 
 function Game() {
-
     return (
-        <>
-            <NavBar/>
-            <div className='h-screen flex justify-center items-center'>Game Page in progress</div>
-        </>
-    )
-}
-
-export default Game;
+      <>
+        <NavBar />
+        <div className="h-screen flex flex-col items-center game-page">
+          <h1 >PONG</h1>
+          <div className="flex flex-col items-center"></div>
+          <div className="flex justify-center items-center">
+            <button className="searchgame-button ">Search Game</button>
+            <div className="net w-100px"></div>
+            <button className="searchgame-button">Invite Friends</button>
+          </div>
+        </div>
+      </>
+    );
+  }
+  
+  export default Game;
+  
