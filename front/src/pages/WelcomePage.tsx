43b0cd52bffa885ba56721b84ac483b3c66e8270
louiseameliemarechal <louiseamelie2@gmail.com>
2023-06-13 13:48:30 +0200

    added axios to the package.json

--- front/src/pages/WelcomePage.tsx
+++ front/src/pages/WelcomePage.tsx
@@ -2,7 +2,6 @@
 import Reveal from '../components/Reveal.tsx'
 import '../style/pages/WelcomePage.css'
 import NavBar from '../components/NavBar.tsx'
-import { Link } from 'react-router-dom'
 
 // NavBar ne sera pas sur cette page, mais plus facile de circuler sur le site comme ca en attendant
 
