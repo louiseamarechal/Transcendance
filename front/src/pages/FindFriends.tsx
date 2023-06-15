7836c0bb4378b1f37dcaf21f0417a95342da33ee
JBouyer <jbouyer@student.42.fr>
2023-06-15 12:25:53 +0200

    friend page done

--- front/src/pages/FindFriends.tsx
+++ front/src/pages/FindFriends.tsx
@@ -1,7 +1,7 @@
 
 import NavBar from '../components/NavBar.tsx';
-import FriendProfil from '../components/FriendProfil.tsx';
 import '../style/pages/FindFriends.css'
+import UserCard from '../components/UserCard.tsx';
 
 
 function FindFriends() {
@@ -9,12 +9,16 @@ function FindFriends() {
     return (
         <>
             <NavBar/>
-            <div className="findfriends-container"> 
-                {/* <div className="findfriendtab"> */}
-                    <FriendProfil/>
-                    <FriendProfil/>
-                    <FriendProfil/>
-               {/* </div> */}
+            <div className="findfriends-container friend-card"> 
+               <div className="friend-card" ><UserCard /> </div>
+               <div className="friend-card" ><UserCard /> </div>
+               <div className="friend-card" ><UserCard /> </div>
+               <div className="friend-card" ><UserCard /> </div>
+               <div className="friend-card" ><UserCard /> </div>
+               <div className="friend-card" ><UserCard /> </div>
+               <div className="friend-card" ><UserCard /> </div>
+               <div className="friend-card" ><UserCard /> </div>
+               <div className="friend-card" ><UserCard /> </div>
                 </div>            
         </>
     )
