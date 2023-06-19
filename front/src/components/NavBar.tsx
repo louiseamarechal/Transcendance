b1b2411fe26ca9e018065ade78a0391c026f6f9c
louiseameliemarechal <louiseamelie2@gmail.com>
2023-06-19 11:26:17 +0200

    when we click on NavBar click - remove the navbar

--- front/src/components/NavBar.tsx
+++ front/src/components/NavBar.tsx
@@ -21,12 +21,12 @@ const NavBar = () => {
                 <FontAwesomeIcon icon={ faXmark } className="opened-nav-button" style={{color:"var(--black)"}} onClick={() => {setNavbarState(false)}}/>
                 {/* <button className={"opened-nav-button fa-solid fa-xmark"} style={{color:"var(--black)"}} onClick={() => {setNavbarState(false)}}></button> */}
                 <ul className="navbar-links">
-                    <Link to={'/profil'}><img className="avatar" alt="avatar" src="https://avatars.dicebear.com/api/adventurer-neutral/mail%40ashallendesign.co.uk.svg" /></Link>
-                    <Link to={'/game'}>Game</Link>
-                    <Link to={'/chat'}>Chat</Link>
-                    <Link to={'/friends'}>Friends</Link>
-                    <Link to={'/settings'}>Settings</Link>
-                    <Link to={'/test'}>Test</Link>
+                    <Link to={'/profil'}><img className="avatar" alt="avatar" src="https://avatars.dicebear.com/api/adventurer-neutral/mail%40ashallendesign.co.uk.svg" onClick={() => {setNavbarState(false)}}/></Link>
+                    <Link to={'/game'} onClick={() => {setNavbarState(false)}}>Game</Link>
+                    <Link to={'/chat'} onClick={() => {setNavbarState(false)}}>Chat</Link>
+                    <Link to={'/friends'} onClick={() => {setNavbarState(false)}}>Friends</Link>
+                    <Link to={'/settings'} onClick={() => {setNavbarState(false)}}>Settings</Link>
+                    <Link to={'/test'} onClick={() => {setNavbarState(false)}}>Test</Link>
                 </ul>
             </div>
         )
