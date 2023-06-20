3c4a1135cda3b909eb95c2e1419eabb33b4e6a74
FearfulOstrich <antoine.alleon@gmail.com>
2023-06-20 18:58:18 +0200

    add seed.ts

---
+++ back/prisma/seed.ts
@@ -0,0 +1,72 @@
+import { FRStatus, PrismaClient } from '@prisma/client';
+import { createUserDto } from 'src/user/dto';
+
+const prisma = new PrismaClient();
+const defaultAvatar: string = 'http://localhost:3000/public/default.jpg';
+
+async function createUser({ login, avatar }: createUserDto) {
+  return await prisma.user.create({
+    data: {
+      login,
+      name: login,
+      avatar,
+    },
+  });
+}
+
+async function createFriendRequest(
+  userId1: number,
+  userId2: number,
+  status: FRStatus,
+) {
+  return await prisma.friendRequest.create({
+    data: {
+      fromId: userId1,
+      toId: userId2,
+      status,
+    },
+  });
+}
+
+async function createChannel(
+  ownerId: number,
+  members: number[],
+  name: string,
+) {
+	const channel = await prisma.channel.create({
+		data: {
+			ownerId,
+			name,
+		},
+	});
+	await prisma.membersOnChannels.createMany({
+		data: {
+			
+		}
+	})
+	return channel;
+}
+
+async function main() {
+  const michel = await createUser({ login: 'Michel', avatar: defaultAvatar });
+  const claude = await createUser({ login: 'Claude', avatar: defaultAvatar });
+  const yves = await createUser({ login: 'Yves', avatar: defaultAvatar });
+  const marcel = await createUser({ login: 'Marcel', avatar: defaultAvatar });
+  const fr1 = await createFriendRequest(michel.id, yves.id, FRStatus.PENDING);
+  const fr2 = await createFriendRequest(
+    claude.id,
+    marcel.id,
+    FRStatus.ACCEPTED,
+  );
+  const channel1 = await createChannel();
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
