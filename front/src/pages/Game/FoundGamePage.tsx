6c616486d851c1568a16b76a1295379b8058b9ab
louiseameliemarechal <louiseamelie2@gmail.com>
2023-06-21 12:00:46 +0200

    changed folder name

--- front/src/pages/Game/FoundGamePage.tsx
+++ front/src/pages/Game/FoundGamePage.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import '../../style/pages/game/FoundGamePage.css'
+import '../../style/pages/game/FoundGamePage.css';
 
 const FoundGamePage = () => {
   return (
@@ -7,12 +7,12 @@ const FoundGamePage = () => {
       <h1>PONG</h1>
       <p>username wants to play with you</p>
       <div className="game-choice">
-        <Link to='/playgame'>
+        <Link to="/playgame">
           <button className="play-game-button-bg flex justify-center items-center">
             <div className="play-game-triangle"></div>
           </button>
         </Link>
-        <Link to='/game'>
+        <Link to="/game">
           <button className="decline-game-bg">X</button>
         </Link>
       </div>
