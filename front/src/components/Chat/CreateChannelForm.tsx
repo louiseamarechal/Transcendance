f3a40c3e3d058757729285e73092a6b13404d83f
aalleon <antoine.alleon@gmail.com>
2023-06-21 11:51:34 +0200

    plop

--- front/src/components/Chat/CreateChannelForm.tsx
+++ front/src/components/Chat/CreateChannelForm.tsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from 'react';
 import { useChatContext } from '../../hooks/useChatContext';
 import '../../style/components/chat/chat-container.css';
 import useAxiosPrivate from '../../hooks/useAxiosPrivate';
-import { useUser } from '../../hooks/useUser';
 
 const CreateChannelForm = () => {
+  const defaultAvatar = 'http://localhost:3000/'
   const axiosPrivate = useAxiosPrivate();
   const { setShowCreateChannel, setShowChannel } = useChatContext();
   const [friends, setFriends] = useState<
@@ -29,31 +29,39 @@ const CreateChannelForm = () => {
     if (!channelName) {
       alert('Channel name must be filled.');
     } else {
-      await axiosPrivate.post('channel', {
+      await axiosPrivate
+        .post('channel', {
           name: channelName,
           avatar: 'plop',
           members: selectedFriends,
-        }).catch((err) => {
-					if (err.status === 409) {
-						setShowCreateChannel(false);
-						setShowChannel(err.statusText.channelId);
-					}
-				});
-      }
+        })
+        .catch((err) => {
+          if (err.status === 409) {
+            setShowCreateChannel(false);
+            setShowChannel(err.statusText.channelId);
+          }
+        });
     }
+  }
 
   return (
-    <>
-      <input
-        className="channel-name"
-        onChange={(event) => setChannelName(event.target.value)}
-      ></input>
+    <div className="channel-form">
+      <div className="form-header">
+        <label>
+          <img src="" />
+          <input type="file" name="avatar" defaultValue={defaultAvatar}></input>
+        </label>
+        <input
+          className="channel-name"
+          onChange={(event) => setChannelName(event.target.value)}
+        ></input>
+      </div>
       <div className="friends-list"></div>
       <button type="submit" className="small-button" onSubmit={handleSubmit}>
         create channel
       </button>
-    </>
+    </div>
   );
-}
+};
 
 export default CreateChannelForm;
