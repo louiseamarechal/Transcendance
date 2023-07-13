de9ef81589a7bfa11bd5008d839f9fe350947aa7
Alexandre <alefranc.pro@mailo.com>
2023-07-13 16:04:34 +0200

    todo ready and start game

--- front/src/pages/GameSocket/GameSocketGame.tsx
+++ front/src/pages/GameSocket/GameSocketGame.tsx
@@ -6,8 +6,45 @@ import { useParams } from 'react-router-dom';
 
 export default function GameSocketLobby() {
   const [pos, setPos] = useState({ x: 0, y: 0 }); // debug
+
+  const [leftPlayer, setLeftPlayer] = useState<string>('');
+  const [rightPlayer, setRightPlayer] = useState<string>('');
+
+  const [leftPaddle, setLeftPaddle] = useState<number>(0);
+  const [rightPaddle, setRightPaddle] = useState<number>(0);
+  const [ball, setBall] = useState<[number, number]>([0, 0]);
+
+  const [score, setScore] = useState<[number, number]>([0, 0]);
+
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const { gameId } = useParams();
+
+  useEffect(() => {
+    type Payload = {
+      player1: string;
+      player2: string;
+      player1Pos: number;
+      player2Pos: number;
+      ballPos: [number, number];
+      score: [number, number];
+    };
+
+    function handleUpdatePos(payload: Payload) {
+      console.log(payload);
+      setLeftPlayer(payload.player1)
+      setRightPlayer(payload.player2)
+      setLeftPaddle(payload.player1Pos)
+      setRightPaddle(payload.player2Pos)
+      setBall(payload.ballPos)
+      setScore(payload.score)
+    }
+
+    socket.on('server.game.updatePos', handleUpdatePos);
+
+    return () => {
+      socket.off('server.game.updatePos', handleUpdatePos);
+    };
+  }, []);
 
   const handlePointerMove = (event: PointerEvent<HTMLDivElement>) => {
     const divMinY = canvasRef.current!.offsetTop;
@@ -31,6 +68,8 @@ export default function GameSocketLobby() {
     socket.emit(ClientEvents.GameInput, payload);
   };
 
+  
+
   return (
     <div
       className="border-4 border-red-700 h-full flex flex-col justify-center items-center"
@@ -41,8 +80,9 @@ export default function GameSocketLobby() {
         <br />
         Pos Y: {pos.y}
       </div>
+        <button className='text-9xl text-red-900'>Ready</button>
       <div className="h-4/5 w-4/5 border-4 border-blue-600">
-        <canvas ref={canvasRef} className="h-full w-full" id="canvas" />
+        {/* <canvas ref={canvasRef} className="h-full w-full" id="canvas" /> */}
       </div>
     </div>
   );
