191e4bf2cb3a28332e4a6093144c77b7ac0ebcc6
Alexandre <alefranc.pro@mailo.com>
2023-07-19 10:43:45 +0200

    start paddle rendering

--- front/src/components/game/GameBackground.tsx
+++ front/src/components/game/GameBackground.tsx
@@ -1,13 +1,9 @@
 function GameBackground() {
   return (
     <div className="absolute border-4 border-pink-700 w-full h-full flex justify-center items-center">
-      <div className="border-2 border-cyan-800 flex-1 w-full">
-        Background placeholder
-      </div>
+      <div className="flex-1 w-full"></div>
       <div className="border-[6px] border-dashed border-[#1d1d1b80] rounded-full flex-shrink h-[90%]"></div>
-      <div className="border-2 border-cyan-800 flex-1 w-full">
-        Background placeholder
-      </div>
+      <div className="flex-1 w-full"></div>
     </div>
   );
 }
