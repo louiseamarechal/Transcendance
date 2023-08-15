3bd94ae68b354335f747431761ae4d2bbec4dec8
louiseameliemarechal <louiseamelie2@gmail.com>
2023-08-15 15:29:35 +0200

    test removed and navbar cleaned

--- front/src/components/NavBarLinks.tsx
+++ front/src/components/NavBarLinks.tsx
@@ -35,15 +35,7 @@ const NavBarLinks = () => {
     { to: '/oldgame', content: 'OldGame' },
     { to: '/chat', content: 'Chat' },
     { to: '/friends', content: 'Friends' },
-    { to: '/test', content: 'Test' },
     { to: '/FindFriends', content: 'FindFriends' },
-    { to: '/profil/1', content: 'Profil 1' },
-    { to: '/profil/2', content: 'Profil 2' },
-    { to: '/profil/3', content: 'Profil 3' },
-    { to: '/profil/4', content: 'Profil 4' },
-    { to: '/profil/5', content: 'Profil 5' },
-    { to: '/profil/6', content: 'Profil 6' },
-    { to: '/profil/7', content: 'Profil 7' },
   ];
 
   if (navbar.navbarState === true) {
