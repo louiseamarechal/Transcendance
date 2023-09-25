1c9782ae5a1d692d74c66d6a871215c667beba41
Alexandre <alefranc.pro@mailo.com>
2023-09-25 14:10:35 +0200

    change random by now in Avatar

--- front/src/components/Avatar.tsx
+++ front/src/components/Avatar.tsx
@@ -10,9 +10,9 @@ export default function Avatar({ id, file, small }: AvatarProps) {
   let url: string;
 
   if (id) {
-    url = `${BACK_URL}/user/avatarById/${id}?random=${Math.random()}`;
+    url = `${BACK_URL}/user/avatarById/${id}?random=${performance.now()}`;
   } else if (file) {
-    url = `${BACK_URL}/user/avatarByFile/${file}?random=${Math.random()}`;
+    url = `${BACK_URL}/user/avatarByFile/${file}?random=${performance.now()}`;
   } else {
     return null;
   }
