6d8a2071f86cc66b18880beedf2073801acf0dda
FearfulOstrich <antoine.alleon@gmail.com>
2023-09-25 21:14:07 +0200

    fix channel name + pass + avatar change + leave channel.

--- front/src/pages/chat/chat-channel/channel-options/ChannelSettings.tsx
+++ front/src/pages/chat/chat-channel/channel-options/ChannelSettings.tsx
@@ -4,11 +4,15 @@ import { createPortal } from 'react-dom';
 import useChannel from '../../../../hooks/useChannel';
 import useAxiosPrivate from '../../../../hooks/useAxiosPrivate';
 import CancelPrompt from '../../../../components/CancelPrompt';
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import useChannelList from '../../../../hooks/useChannelList';
+import { ChannelShort } from '../../../../types/Channel.type';
 
 export default function ChannelSettings() {
   const axiosPrivate = useAxiosPrivate();
   const channelState = useChannel();
+  const navigate = useNavigate();
+  const channelListState = useChannelList();
   const { myId } = useUser();
   const [nameEdit, setNameEdit] = useState<string>('');
   const [avatarEdit, setAvatarEdit] = useState<string>('');
@@ -16,6 +20,7 @@ export default function ChannelSettings() {
   const [showModal, setShowModal] = useState<boolean>(false);
   const [searchParams] = useSearchParams();
   const isDM: boolean = searchParams.get('isDM') === 'true';
+  const isOwner: boolean = channelState.self.ownerId === myId;
 
   async function changeChannelName() {
     if (!nameEdit || nameEdit === '') {
@@ -26,8 +31,16 @@ export default function ChannelSettings() {
           name: nameEdit,
         })
         .then((res) => {
-          console.log('changing name.');
           channelState.reset({ ...channelState.self, name: res.data.name });
+          channelListState.reset(
+            channelListState.self.map((ch: ChannelShort) => {
+              if (ch.id === channelState.self.id) {
+                return { ...ch, name: res.data.name };
+              } else {
+                return ch;
+              }
+            }),
+          );
         });
     }
   }
@@ -42,6 +55,15 @@ export default function ChannelSettings() {
         })
         .then((res) => {
           channelState.reset({ ...channelState.self, avatar: res.data.avatar });
+          channelListState.reset(
+            channelListState.self.map((ch: ChannelShort) => {
+              if (ch.id === channelState.self.id) {
+                return { ...ch, avatar: res.data.avatar };
+              } else {
+                return ch;
+              }
+            }),
+          );
         });
     }
   }
@@ -50,28 +72,77 @@ export default function ChannelSettings() {
     if (!passwordEdit || passwordEdit === '') {
       alert('Cannot have empty password for channel.');
     } else {
-      await axiosPrivate.patch(`channel/${channelState.self.id}`, {
-        password: passwordEdit,
-      });
+      await axiosPrivate
+        .patch(`channel/${channelState.self.id}`, {
+          password: passwordEdit,
+        })
+        .catch((e) => {
+          console.error(e);
+        });
+      setPasswordEdit('');
     }
   }
 
   async function deleteChannel() {
     setShowModal(false);
-    await axiosPrivate.delete(`channel/${channelState.self.id}`).then((_) => {
-      console.log(`channel ${channelState.self.id} deleted`);
-    });
+    await axiosPrivate
+      .delete(`channel/${channelState.self.id}`)
+      .then((_) => {
+        console.log(`channel ${channelState.self.id} deleted`);
+        channelListState.reset(
+          channelListState.self.filter((ch) => ch.id !== channelState.self.id),
+        );
+        navigate('/chat');
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  }
+
+  async function leaveChannel() {
+    console.log(`Member ${myId} left channel ${channelState.self.name}`);
+    setShowModal(false);
+    await axiosPrivate
+      .delete(`channel/member/${channelState.self.id}/${myId}`)
+      .then(() => {
+        channelListState.reset(
+          channelListState.self.filter((ch) => ch.id !== channelState.self.id),
+        );
+        navigate('/chat');
+      })
+      .catch((e) => console.error(e));
   }
 
   if (isDM) {
     return <div />;
   } else if (
-    channelState.self.ownerId !== myId &&
+    !isOwner &&
     !channelState.self.admins.some(
       (admin: { userId: number }) => admin.userId === myId,
     )
   ) {
-    return <p>Contact channel owner or admin for admin rights.</p>;
+    return (
+      <>
+        <div>
+          <p>Contact channel owner or admin for admin rights.</p>
+          <button
+            className="small-button leave-channel-button"
+            onClick={() => setShowModal(true)}
+          >
+            Leave channel
+          </button>
+        </div>
+        {showModal &&
+          createPortal(
+            <CancelPrompt
+              message={'leave channel?'}
+              onContinue={leaveChannel}
+              onCancel={() => setShowModal(false)}
+            />,
+            document.body,
+          )}
+      </>
+    );
   } else {
     return (
       <>
@@ -113,7 +184,7 @@ export default function ChannelSettings() {
           ) : (
             <></>
           )}
-          {channelState.self.ownerId === myId ? ( // Case for admin only
+          {isOwner ? ( // Case for admin only
             <button
               className="small-button"
               id="delete-channel-button"
@@ -122,14 +193,19 @@ export default function ChannelSettings() {
               Delete channel
             </button>
           ) : (
-            <></>
+            <button
+              className="small-button leave-channel-button"
+              onClick={() => setShowModal(true)}
+            >
+              Leave channel
+            </button>
           )}
         </div>
         {showModal &&
           createPortal(
             <CancelPrompt
-              message={'delete channel?'}
-              onContinue={deleteChannel}
+              message={isOwner ? 'delete channel?' : 'Leave channel?'}
+              onContinue={isOwner ? deleteChannel : leaveChannel}
               onCancel={() => setShowModal(false)}
             />,
             document.body,
